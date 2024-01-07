1c714ca8924a92327b97f74703c51d00b08a1baa
Ziyad Derfoufi <zderfouf@e2r11p4.1337.ma>
2024-01-07 19:43:20 +0100

    first clone understanding how read behaves

--- get_next_line.h
+++ get_next_line.h
@@ -1,6 +1,13 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<fcntl.h>
+
+#define BUFFER_SIZE 10
+
 char *get_next_line(int fd);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,6 +1,70 @@
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+int ft_strlen(char *str)
 {
+    int i;
+
+    i = 0;
+    if(!str)
+        return(0);
+    while (str[i])
+        i++;
+    return (i);
+}
+
+char    *ft_strjoin(char *s1, char *s2)
+{
+    int     i;
+    int     j;
+    char    *str;
+
+    i = 0;
+    j = 0;
+    if (!s1 || !s2)
+        return(NULL);
+    if (!(str = (char *) malloc (ft_strlen(s1) + ft_strlen(s2) + 1)))
+        return(NULL);
+    while(s1[i])
+    {
+        str[i] = s1[i];
+        i++;
+    }
+    while (s2[j])
+    {
+        str[i + j] = s2[j];
+        j++;
+    }
+    str[i + j] = 0;
+    return (str);
+}
+char    *ft_strdup(char *s)
+{
+    int     i;
+    char    *str;
+
+    i = 0;
+    if (!s)
+        return (NULL);
+    if (!(str = (char *) malloc (ft_strlen(s) + 1)))
+        return("NULL");
+    while (s[i])
+    {
+        str[i] = s[i];
+        i++;
+    }
+    return(str);
+}
+int main()
+{
+    ssize_t rd;
+    static char a[20];
+    int fd = open("file.txt", O_RDWR | O_CREAT , 0777);
     
+    printf("%d\n",fd);
+    rd = 1;
+    while (rd > 0)
+    {
+        rd = read(fd,a,BUFFER_SIZE);
+        printf("%s",a);
+    }
 }
